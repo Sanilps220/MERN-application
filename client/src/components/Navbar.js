8d12a5d829d86c9bc065502c568e94044f0a1ffd
Sanil ps <97731039+Sanilps220@users.noreply.github.com>
2022-11-25 22:58:36 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,25 @@
 import React ,{useContext}from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {UserContext} from '../App'
+
 function Navbar() {
+  const history =  useNavigate()
   const {state,dispatch} = useContext(UserContext)
   console.log(state);
+  const logoutUser=() =>{ 
+    sessionStorage.clear();
+    dispatch({ type: "CLEAR" });
+    history('/')
+  }
   const renderList= () =>{
+
     if(state){
       return[    
         <li><Link to="/profile" >Profile</Link></li>,
-        <li><Link to="/post" >Create post</Link></li>     
+        <li><Link to="/post" >Create post</Link></li>   ,
+        <li className='btn-out'
+        onClick={() => logoutUser()}>Logout</li>   
+
        ]
     }else{
       return[
