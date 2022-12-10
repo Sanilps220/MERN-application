4ad63165928a8f412c4d95e24190d59c63fecffa
Sanil ps <97731039+Sanilps220@users.noreply.github.com>
2022-12-10 11:07:07 +0530

    Update keys.js

--- Server/keys.js
+++ Server/keys.js
@@ -1,14 +1,7 @@
-// eU466KHC9jzvv7Kj
-// h86XGm6swiXEEodh
+
 module.exports={
-    MONGOURL: "mongodb+srv://sanil1:h86XGm6swiXEEodh@cluster0.hwbamwm.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET:"329f5530d6d7e55f"
+    MONGOURL: "",
+    JWT_SECRET:""
  }   
     
    
-
-//"mongodb+srv://Sanil:eU466KHC9jzvv7Kj>@cluster0.hwbamwm.mongodb.net/?retryWrites=true&w=majority"
-//Pwk3SDHJR9kyZLWW
-// module.exports = {
-//    :"mongodb+srv://sanil:Pwk3SDHJR9kyZLWW@cluster0.7dtiauy.mongodb.net/?retryWrites=true&w=majority"
-// } 
